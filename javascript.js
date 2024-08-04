85fb36f8a2d435d1cc8b3c06fb717f5e28ce5dc1
wroughtironkite <175876258+wroughtironkite@users.noreply.github.com>
2024-08-04 06:55:31 -0400

    Add primary game and round loops

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //create new function getComputerChoice
 function getComputerChoice() {
-    //obtain random number between 0 and 1 and multiply by 3
+    //obtain random number "x" between 0 and 1 and multiply by 3
     let x = Math.floor(Math.random()*3);
     //if random number is less than 1, return rock
     if (x < 1) return "rock";
@@ -12,6 +12,7 @@ function getComputerChoice() {
 
 //create new function getPlayerChoice
 function getPlayerChoice() {
+    //create temporary variable "y"
     let y = 0; 
     while (y != "r" && y != "rock" && y != "s" && y != "scissors" && y != "p" && y != "paper") {
         //prompt user to pick from rock, paper, and scissors
@@ -29,33 +30,53 @@ function getPlayerChoice() {
 }
 
 //create new function updateScore
-function updateScore() {
+function updateScore(PlayerChoice, ComputerChoice) {
     //if player chose rock and comp chose scissors || player chose scissors and comp chose paper || player chose paper and comp chose scissors
+        
+    if (PlayerChoice == "rock" && ComputerChoice == "scissors" || PlayerChoice == "scissors" && ComputerChoice == "paper" || PlayerChoice == "paper" && ComputerChoice == "scissors") {
+        console.log("You win!");
         //increase playerScore by 1
+        playerScore++;
+    }
+    
     //otherwise, increase compScore by 1
+
+    else {
+        console.log("You lose. :(");
+        compScore++;
+    }
+
 }
 
 //create new function playRound
 function playRound() {
+    //create temporary variable "PlayerChoice" and "ComputerChoice"
+    let PlayerChoice = 0;
+    let ComputerChoice = 0;
     //loop the below as long as player and computer choose the same thing
+    while (PlayerChoice == ComputerChoice) {
         //call getPlayerChoice
+        PlayerChoice = getPlayerChoice();
         //call getComputerChoice
+        ComputerChoice = getComputerChoice ();
         //output text "You chose x, Computer chose y"
-    
+        console.log("You chose " + PlayerChoice + ", and the computer chose " + ComputerChoice + ".");
+    }
     //call updateScore
+    updateScore(PlayerChoice, ComputerChoice);
 }
 
 //create function to play a game of five rounds
 function playGame() {
-    //set initial playerScore and compScore to 0
-    
     //loop until five rounds have been played
     for (let roundnumber = 0; roundnumber < 5; roundnumber++) {
-        //call playRound
+        playRound();
     }
-
-    //report final score
 }
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+//set initial playerScore and compScore to 0
+let playerScore = 0;
+let compScore = 0;
+playGame();
+//report final score
+console.log("Out of 5 games, you won " + playerScore + " games and the computer won " + compScore + " games.");
