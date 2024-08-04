d22787c2cf6dd1f23d23d0d71477d5f8900c00fe
wroughtironkite <175876258+wroughtironkite@users.noreply.github.com>
2024-08-03 21:16:59 -0400

    Create base files to work from

---
+++ javascript.js
@@ -0,0 +1,27 @@
+//create new function getComputerChoice
+function getComputerChoice() {
+
+}
+
+//create new function getPlayerChoice
+function getPlayerCHoice() {
+
+}
+
+//create new function updateScore
+function updateScore() {
+
+}
+
+//create new function playRound
+function playRound() {
+
+}
+
+//create loop function to play five rounds
+function playGame() {
+    for (let roundnumber = 0; roundnumber < 5; roundnumber++) {
+        console.log(roundnumber);
+    }
+}
+
